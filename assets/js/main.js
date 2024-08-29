58a394959194093c8a786620766327f96081e819
KesavaTrinadh <141848683+KesavaTrinadh@users.noreply.github.com>
2024-08-29 23:12:39 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,126 +1,101 @@
-/*================= SHOW MENU =================*/
-const navMenu = document.getElementById('nav-menu'),
-navToggle = document.getElementById('nav-toggle'),
-navClose = document.getElementById('nav-close');
+document.addEventListener('DOMContentLoaded', () => {
+    /*================= SHOW MENU =================*/
+    const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
 
-/*================= MENU SHOW =================*/
-/* VALIDATE IF CONSTANT EXISTS */
-if(navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu');
-    });
-}
+    /*================= MENU SHOW =================*/
+    if (navToggle) {
+        navToggle.addEventListener('click', () => {
+            navMenu.classList.add('show-menu');
+        });
+    }
 
-/*================= MENU HIDDEN =================*/
-/* VALIDATE IF CONSTANT EXISTS */
-if(navClose) {
-    navClose.addEventListener('click', () => {
+    /*================= MENU HIDDEN =================*/
+    if (navClose) {
+        navClose.addEventListener('click', () => {
+            navMenu.classList.remove('show-menu');
+        });
+    }
+
+    /*================= REMOVE MENU MOBILE =================*/
+    const navLinks = document.querySelectorAll('.nav__link');
+
+    function linkAction() {
         navMenu.classList.remove('show-menu');
-    });
-}
+    }
 
-/*================= REMOVE MENU MOBILE =================*/
-const navLink = document.querySelector('.nav__link');
+    navLinks.forEach((n) => n.addEventListener('click', linkAction));
 
-function linkAction() {
-    const navMenu = document.getElementById('nav-menu');
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu');
-}
+    /*================= SCROLL SECTIONS ACTIVE LINK =================*/
+    // Add this if you want to add 'active-link' class based on scroll
 
-navLink.forEach((n) => n.addEventListener('click', linkAction));
+    /*================= CHANGE BACKGROUND HEADER =================*/
+    function scrollHeader() {
+        const header = document.getElementById('header');
+        if (this.scrollY >= 80) header.classList.add('scroll-header');
+        else header.classList.remove('scroll-header');
+    }
 
-/*================= SCROLL SECTIONS ACTIVE LINK =================*/
+    window.addEventListener('scroll', scrollHeader);
 
-/*================= CHANGE BACKGROUND HEADER =================*/
-function scrollHeader() {
-    const header = document.getElementById('header');
+    /*================= SHOW SCROLL UP =================*/
+    function scrollUp() {
+        const scrollUp = document.getElementById('scroll-up');
+        if (this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+        else scrollUp.classList.remove('show-scroll');
+    }
 
-    // When the Scroll is greater than 80 viewport height, add the scroll-header class to header tag 
+    window.addEventListener('scroll', scrollUp);
 
-    if(this.scrollY >= 80) header.classList.add('scroll-header');
-    else header.classList.remove('scroll-header');
-}
+    /*================= ABOUT TABS =================*/
+    const tabs = document.querySelectorAll('[data-target]'), 
+    tabContents = document.querySelectorAll('[data-content]');
 
-window.addEventListener('scroll', scrollHeader);
-
-/*================= SHOW SCROLL UP =================*/
-function scrollUp() {
-    const scrollUp = document.getElementById('scroll-up');
-
-    // When the Scroll is greater than 350 viewport height, add the show-scroll class to scroll-top class
-    
-    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll');
-    else scrollUp.classList.remove('show-scroll');
-}
-
-window.addEventListener('scroll', scrollUp);
-
-/*================= ABOUT TABS =================*/
-
-const tabs = document.querySelectorAll('[data-target]'), tabContents = document.querySelectorAll('[data-content]');
-
-tabs.forEach((tab) => {
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.target);
-        /*console.log(target);*/
-        
-        tabContents.forEach((tabContent) => {
-            tabContent.classList.remove('tab__active');
+    tabs.forEach((tab) => {
+        tab.addEventListener('click', () => {
+            const target = document.querySelector(tab.dataset.target);
+            tabContents.forEach((tabContent) => {
+                tabContent.classList.remove('tab__active');
+            });
+            target.classList.add('tab__active');
+            tabs.forEach((tab) => {
+                tab.classList.remove('tab__active');
+            });
+            tab.classList.add('tab__active');
         });
-        
-        target.classList.add('tab__active');
-        
-        tabs.forEach((tab) => {
-            tab.classList.remove('tab__active');
-        });
-
-        tab.classList.add('tab__active');
-        
     });
+
+    /*================= CONTACT FORM =================*/
+    const contactForm = document.getElementById('contact-form'), 
+    contactName = document.getElementById('contact-name'), 
+    contactEmail = document.getElementById('contact-email'),
+    contactSubject = document.getElementById('contact-subject'),
+    contactMessage = document.getElementById('contact-message'),
+    errorMessage = document.getElementById('error-message');
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        if (contactName.value === '' || contactEmail.value === '' || contactSubject.value === '' || contactMessage.value === '') {
+            errorMessage.textContent = 'Write all the required fields';
+        } else {
+            emailjs.sendForm('service_p6lc7qj', 'template_vapmc0c', '#contact-form', 'zY3wZZ-JQ56kHvKzW').then(() => {
+                errorMessage.classList.add('color-first');
+                errorMessage.textContent = 'Message sent ✅';
+                setTimeout(() => {
+                    errorMessage.textContent = '';
+                }, 5000);
+            }, (error) => {
+                alert('Oops! Something went wrong...', error);
+            });
+
+            contactName.value = '';
+            contactEmail.value = '';
+            contactSubject.value = '';
+            contactMessage.value = '';
+        }
+    };
+
+    contactForm.addEventListener('submit', sendEmail);
 });
-
-/*================= CONTACT FORM =================*/
-const contactForm = document.getElementById('contact-form'), 
-contactName = document.getElementById('contact-name'), 
-contactEmail = document.getElementById('contact-email'),
-contactSubject = document.getElementById('contact-subject'),
-contactMessage = document.getElementById('contact-message'),
-errorMessage = document.getElementById('error-message');
-
-const sendEmail = (e) => {
-    e.preventDefault();
-
-    // Check if the field has a value
-
-    if(contactName.value === '' ||  contactEmail.value === '' || contactSubject.value === '' || contactMessage.value === '' ) {
-        // Show message
-        errorMessage.textContent = 'Write all the required fields'
-    }
-
-    else {
-        // ServiceID - templateID - #form - publickey
-        emailjs.sendForm('service_p6lc7qj', 'template_vapmc0c', '#contact-form', 'zY3wZZ-JQ56kHvKzW') .then(() => {
-            // Show message and add color, window + dot to open emoji
-            errorMessage.classList.add('color-first');
-            errorMessage.textContent = 'Message sent ✅';
-
-            // Remove Message after 5 seconds
-            setTimeout(() => {
-                errorMessage.textContent = '';
-            }, 5000);
-        }, 
-        (error) => {
-            alert('OOPs! SOMETHING WENT WRONG...', error);
-        });
-
-        // Clear Input Field
-        contactName.value = '';
-        contactEmail.value = '';
-        contactSubject.value = '';
-        contactMessage.value = '';
-    }
-};
-
-contactForm.addEventListener('submit', sendEmail);
-
